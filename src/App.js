842c5f6614debe41e6c92e9d4584e11b38df9446
Mark Edward Ratio <markedwardratio@gmail.com>
2021-02-14 13:52:20 +0800

    Update App class component to be function component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React, { Component } from 'react';
-
 import CV from './components/CV';
 
-class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
-
-	render() {
-		return (
-			<main>
-				<div className="container my-4">
-					<div className="row justify-content-center">
-						<CV />
-					</div>
+function App() {
+	return (
+		<main>
+			<div className="container my-4">
+				<div className="row justify-content-center">
+					<CV />
 				</div>
-			</main>
-		);
-	}
+			</div>
+		</main>
+	);
 }
 
 export default App;
